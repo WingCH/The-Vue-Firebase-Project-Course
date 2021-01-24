c6db83598fee0b8d18741e53eb75c6592132df1a
WingCH <jason021075@me.com>
2021-01-24 13:48:35 +0800

    done 9

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,10 +5,12 @@
       <template v-slot:user="{ user }">
         <div v-if="user">
           <UserProfile :user="user"></UserProfile>
+          <ChatList :uid="user.uid"></ChatList>
         </div>
         <Login v-else />
       </template>
     </User>
+    
   </div>
 </template>
 
@@ -16,12 +18,14 @@
 import Login from "./Login";
 import User from "./User";
 import UserProfile from "./UserProfile";
+import ChatList from "./ChatList";
 
 export default {
   components: {
     Login,
     User,
     UserProfile,
+    ChatList,
   },
 };
-</script>
+</script >
